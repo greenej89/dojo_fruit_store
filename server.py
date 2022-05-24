cf7758ce7de186838bc5824998a4e0f6fcbcf4a2
Jessica Greene <jessica.greene89@gmail.com>
2022-05-24 12:04:35 -0400

    completed dojo_fruits assignment

--- server.py
+++ server.py
@@ -8,11 +8,25 @@ def index():
 @app.route('/checkout', methods=['POST'])         
 def checkout():
     print(request.form)
-    return render_template("checkout.html")
+    strawberry = int(request.form['strawberry'])
+    raspberry = int(request.form['raspberry'])
+    apple = int(request.form['apple'])
+    count = strawberry + raspberry + apple
+
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    customer_name = f"{first_name} {last_name}" 
+
+    print(f"Charging {customer_name} for {count} fruits")
+
+    # DO NOT RENDER TEMPLATE ON ROUTES WITH POST METHOD AFTER THIS ASSIGNMENT.
+    return render_template("checkout.html", strawberry=strawberry, raspberry=raspberry, apple = apple, count = count,
+                            first_name = first_name, last_name = last_name, student_id = request.form['student_id'])
 
 @app.route('/fruits')         
 def fruits():
     return render_template("fruits.html")
 
 if __name__=="__main__":   
-    app.run(debug=True)    
+    app.run(debug=True)  
+    # app.run(debug=True, port=5001)   #For Mac users who use Google Chrome
